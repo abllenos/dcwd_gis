a8d7fdc524921000f7c796c8d56d71b6f0b471c6
abllenos <167511081+abllenos@users.noreply.github.com>
2025-03-25 16:41:29 +0800

    new update

--- dcwd_gis_mgt/src/index.tsx
+++ dcwd_gis_mgt/src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './AuthContext';
 import App from './App';
 
-ReactDOM.render(
-  <Router>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </Router>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <Router>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Router>
+  </React.StrictMode>
 );
